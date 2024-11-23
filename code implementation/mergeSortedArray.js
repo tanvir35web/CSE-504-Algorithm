f42c8ab5396e6b2a4fc9cf11cd961766a00b15f2
tanvir35web <islammdtanvirul@gmail.com>
2024-11-23 12:14:42 +0600

    another approach to solve merge sorted array

--- code implementation/mergeSortedArray.js
+++ code implementation/mergeSortedArray.js
@@ -1,33 +1,32 @@
-function mergeSortedArray(arr1, arr2) {
+const mergeTwoLists = function(list1, list2){
     const mergeArray = [];
     let i = 0;
     let j = 0;
 
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            mergeArray.push(arr1[i]);
+    while (i < list1.length && j < list2.length) {
+        if (list1[i] < list2[j]) {
+            mergeArray.push(list1[i]);
             i++;
         } else {
-            mergeArray.push(arr2[j]);
+            mergeArray.push(list2[j]);
             j++;
         }
     }
 
-    while (i < arr1.length) {
-        mergeArray.push(arr1[i]);
+    while (i < list1.length) {
+        mergeArray.push(list1[i]);
         i++;
     }
 
-    while (i < arr2.length) {
-        mergeArray.push(arr2[j]);
+    while (i < list2.length) {
+        mergeArray.push(list2[j]);
         j++;
     }
 
     return mergeArray;
 }
 
-const arr1 =[1, 3, 5, 7];
-const arr2 = [2, 4, 6, 8];
-
-const mergeArray = mergeSortedArray(arr1, arr2);
+const list1 =[1,2,4]
+const list2 = [1,3,4]
+const mergeArray = mergeTwoLists(list1, list2);
 console.log("Merged Sorted Array: ", mergeArray);
